0a53c0dc63bd021849e9369860a56c42d16ed9ed
juanma1980 <juanma1980@gmail.com>
2020-01-23 10:17:24 +0000

    Multiline support

--- python3-QAnimatedStatusBar/edupals/ui/QAnimatedStatusBar.py
+++ python3-QAnimatedStatusBar/edupals/ui/QAnimatedStatusBar.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
-from PyQt5.QtWidgets import QStatusBar,QPushButton
+from PyQt5.QtWidgets import QStatusBar,QPushButton,QLabel
 from PyQt5 import QtGui
-from PyQt5.QtCore import Qt, QPropertyAnimation,QRect,QTimer,QSize
+from PyQt5.QtCore import Qt, QPropertyAnimation,QRect,QTimer
 
 class QAnimatedStatusBar(QStatusBar):
 
 	def __init__(self):
 		super().__init__()
-		self.dbg=False
+		self.dbg=True
 		self.height_=0
 		self.anim=QPropertyAnimation(self, b"geometry")
 		self.hide()
 		self.timer=QTimer()
 		self.timer.setSingleShot(True)
-		icon=QtGui.QIcon.fromTheme("window-close")
-		self.btn_close=QPushButton()
-		self.btn_close.setIcon(icon)
-		self.btn_close.setIconSize(QSize(32,32))
-		self.btn_close.setStyleSheet("background-color:transparent;margin:0px;border:0px;")
+		self.btn_close=QPushButton("X")
 		self.insertPermanentWidget(0,self.btn_close)
 		self.btn_close.clicked.connect(self._hide_message)
 		self.btn_close.setAutoDefault(False)
 		self.btn_close.setDefault(False)
 		self.animationInterval=1000
+		self.label=QLabel("")
+		self.label.setStyleSheet("background-color:transparent;margin:0px;border:0px;text-decoration:none")
+		self.addWidget(self.label)
 		self.showInterval=3000
 		self.msg=''
 		self.css={}
@@ -35,7 +34,8 @@ class QAnimatedStatusBar(QStatusBar):
 	#def _debug
 
 	def setText(self,msg):
-		self.msg="%s"%msg
+			#self.msg="%s"%msg.replace("\n","<br>")
+		self.label.setText("%s"%msg.replace("\n","<br>"))
 	#def setText
 
 	def setAnimation(self,animation):
@@ -72,24 +72,26 @@ class QAnimatedStatusBar(QStatusBar):
 		else:
 				self.setStyleSheet("""%s"""%self.css['default'])
 		self.raise_()
-		self.showMessage("%s"%self.msg)
+		#self.showMessage("%s"%self.msg)
+		self.showMessage("")
 		self.anim.setDuration(self.animationInterval)
 		self.anim.setLoopCount(1)
-		height=(self.height()/10)-10
-		if self.height_<height:
-			self.height_=height
+		self.btn_close.height=24
 		super(QAnimatedStatusBar,self).show()
 		width_=self.parentWidget().width()
 		#360px should be good enough....
 		width=360
 		self.anim.setDuration(1)
 		self.anim.setStartValue(QRect(width_-width,0,width,0))
-		self.anim.setEndValue(QRect(width_-width,0,width,self.height_))
+		self.anim.setEndValue(QRect(width_-width,0,width,self.label.height()+6))
 		self.anim.start()
 		if state==None or state=='':
 			self._debug("Hide state %s"%state)
+			height=(self.height()/10)-10
+			self.height_=height
 			self.timer.singleShot(self.showInterval, lambda:self._hide_message())
-	#def show
+	#def show(self,state=None):
+	
 
 	def _hide_message(self):
 		self._debug("Hiding statusbar")
@@ -100,4 +102,3 @@ class QAnimatedStatusBar(QStatusBar):
 		self.anim.setEndValue(QRect(width_-width,0,width,0))
 		self.anim.start()
 		self.timer.singleShot(self.animationInterval, lambda:self.hide())
-	#def _hide_message
